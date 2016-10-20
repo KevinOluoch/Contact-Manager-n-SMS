8b704b566e40c80fa6ccb8bf44668b242d4f9612
KevinOluoch <kevin.o.oluoch@gmail.com>
2016-10-20 15:52:25 +0300

    Compliance with PEP 8, no internet access bug fixed

--- sms.py
+++ sms.py
@@ -1,21 +1,38 @@
-# Import the helper gateway class
+# Originally copied from
+# Edited to fit its purpose in the code
+# Importing the helper gateway class
 from AfricasTalkingGateway import AfricasTalkingGateway, AfricasTalkingGatewayException
-# Specify your login credentials
-def text(receiving_number, message):
-	username = "kevinoluoch"
-	apikey   = "4fa7ca9b35ecc411875512ee24c42318b756d6cff5c575d4dca73e8b686e1974"
 
+
+def text(receiving_number, message):
+	"""
+	This function takes a phone number and message as input,
+	then attempts to send the message to the given number.
+	It then returns feedback on the attempt.
+	This could be:
+	              Success: in sendin the message,
+	              Invalid Phone Number: if number doesn't exist or is wrongly formatted
+	              No Internet connection"""
+
+
+    # The login credentials
+	username = "kevinoluoch"
+	apikey = "4fa7ca9b35ecc411875512ee24c42318b756d6cff5c575d4dca73e8b686e1974"
+
+    # The number receiving the text is prefixed with a +
 	receiving_number = "+" + str(receiving_number)
-	#to      = "+254711835117"
-	# Create a new instance of our awesome gateway class
+	
+	# Creating a new instance of the gateway class in AfricasTalkingGateway
 	gateway = AfricasTalkingGateway(username, apikey)
 
-    # Any gateway errors will be captured by our custom Exception class below, 
-    # so wrap the call in a try-catch block
+    # Any gateway errors will be captured by the custom Exception class in AfricasTalkingGateway , 
 	try:
-		results = gateway.sendMessage(receiving_number, message)
+		results = gateway.sendMessage(receiving_number, message,)
 
-	except AfricasTalkingGatewayException, e:
-		print 'Encountered an error while sending: %s' % str(e)
+	except AfricasTalkingGatewayException, e: 
+		results = [{'status' : 'Encountered an error while sending: %s' % str(e)}]
+
+	except: # Captures internet connection error
+	    results = [{'status' : "Message Not Sent: No internet connection"}]
 
 	return results
