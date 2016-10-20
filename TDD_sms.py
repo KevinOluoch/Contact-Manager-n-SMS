8cfd699780070998bdb386fce06da795d5388f7d
KevinOluoch <kevin.o.oluoch@gmail.com>
2016-10-20 21:47:26 +0300

    Corrected test messages

--- TDD_sms.py
+++ TDD_sms.py
@@ -10,12 +10,18 @@ class TestGlobalFunctions(unittest.TestCase):
     def testsms_send(self):
         SMS = sms.text(254711835117, "Hi Everyone")
         print
-        self.assertEqual(SMS[0]['status'], "Success", msg="The function should return the word 'Success' after successfully sending a text")
+        self.assertEqual(SMS[0]['status'], "Success", 
+            msg="""The function should return the word 'Success' 
+            after successfully sending a text""")
 
 
     def testtext_send_to_wrong_number(self):
         SMS = sms.text(123456, "Hi Everyone")
-        self.assertEqual(SMS[0]['status'], "Invalid Phone Number", msg="The function should return the number if it is not divisible by 3 or 5")
+        self.assertEqual(SMS[0]['status'], "Invalid Phone Number", 
+            msg="""The function should return the phrase 'Invalid Phone Number'
+             if the Phone Number is not a valid phone number""")
+             
+
 
 if __name__ == '__main__':
     unittest.main()
